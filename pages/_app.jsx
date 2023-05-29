7c806c7fd3c960057b4f39701fa9b67ef98a25db
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2023-05-29 09:06:59 +0200

    connectkit 1.3.0

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle, theme } from '../components/GlobalStyle'
 
-import { WagmiConfig, createClient, chain } from "wagmi";
+import { WagmiConfig, createClient } from "wagmi";
 import { ConnectKitProvider, getDefaultClient } from "connectkit";
 
 const client = createClient(
   getDefaultClient({
     appName: "ConnectKit Next.js demo",
     alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
-    chains: [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum]
-    // walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
   })
 );
 
